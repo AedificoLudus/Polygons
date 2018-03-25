338b06db424e60046daa9d00499911c089214cdc
Kyle McNerdFace <overclockedsanic@protonmail.com>
2018-03-25 15:54:23 +0300

    Update MyPolygons.h

--- MyPolygons.h
+++ MyPolygons.h
@@ -1,33 +1,27 @@
-#ifndef MyPolygons_h
-#define MyPolygons_h
-
 #include "Polygon.h"
 
-class MyPolygons {
-private:
+struct mnode
+{
+  Point *data;
+  mnode *next;
+  mnode *prev;
+}
+
+class MyPolygons
+{
 public:
+    mnode *current;
+  
+    int counter = 0;
 
-  //Declare Node
-  struct mnode
-  {
-    Point *data;
-    struct mnode *next;
-    struct mnode *prev;
-  } *current;
-
-
-  int counter = 0;
-
-  mnode* create_mnode(Polygon* data);
-  void reset();
-  void prepend(Polygon* data);
-  void insert_last(Polygon* data);
-  void insert_pos(Polygon* data, int pos);
-  void delete_pos(Polygon* data, int pos);
-  void add_first();
-  void swap(mnode);
-  Polygon* head();
-  std::string to_string();
+    mnode* create_mnode(Polygon* data);
+    void reset();
+    void prepend(Polygon*);
+    void insert_last(Polygon*);
+    void insert_pos(Polygon*, int pos);
+    void delete_pos(Polygon*, int pos);
+    void add_first();
+    void swap(mnode);
+    Polygon* head();
+    std::string to_string();
 };
-
-#endif
