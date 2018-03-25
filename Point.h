9d76e313bfcb6797e654619c228a9fee7baa04f6
Kyle McNerdFace <overclockedsanic@protonmail.com>
2018-03-25 15:43:30 +0300

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,16 +1,12 @@
-#ifndef Point_h
-#define Point_h
+#include <string>
 
-#include<string>
-
-class Point {
-  private:
-    double xCoord, yCoord;
-  public:
-    void set_values(double, double);
-    double get_value(bool);
-    double distance();
-    std::string to_string();
+class Point
+{
+private:
+  double xCoord, yCoord;
+public:
+  void set_values(double, double);
+  double get_value(bool);
+  double distance();
+  std::string to_string();
 };
-
-#endif
