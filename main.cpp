a588438fb754639363cd34d3604c5a6170a3236d
tomw <aeduscreative@gmail.com>
2018-03-26 04:52:01 +1100

    plan for main,cpp commented in

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
-#include <sstream>
-#include <fstream>
-
+#include "includes"
 #include "MyPolygons.h"
 
 std::ifstream infile();
 
 int main()
 {
-  MyPolygons input;
-  input.add_first();
+  //open file
+  //make MyPolygons
+  MyPolygons firstSet = new MyPolygons
+  firstSet.add_first()
+  //read file
+  //create Polygons in MyPolygons
+  //copy MyPolygons
+  MyPolygons secondSet = firstSet
+  //sort new copy
+  secondSet.sort();
+  //print both
+  firstSet.to_string();
+  secondSet.to_string();
 }
